5e1c1e3a45447c21e2eb66b0c71df85c877c0051
Andrew Smith <andsmi@microsoft.com>
2023-07-29 23:32:28 -0700

    [ci]: add threshold for pass or fail (#26)
    ## Guidelines
    
    * **Please check if the PR fulfills these requirements**
    - [ ] The commit message follows our guidelines
    - [ ] Tests for the changes have been added (for bug fixes/features)
    - [ ] Docs have been added / updated (for bug fixes / features)
    
    
    * **What kind of change does this PR introduce?** (Bug fix, feature,
    docs update, ...)
    
    
    * **What is the current behavior?** (You can also link to an open issue
    here)
    
    
    * **What is the new behavior (if this is a feature change)?**
    
    
    * **Does this PR introduce a breaking change?** (What changes might
    users need to make in their application due to this PR?)
    
    
    * **Other information**:
    
    ## Pull Request Title Format
    
    See the [contributing guide](/CONTRIBUTING.md) for details.

--- test/scripts/ci.sh
+++ test/scripts/ci.sh
@@ -1,3 +1,21 @@
-export WANDB_MODE=dryrun
-python -m RecurrentFF.benchmarks.mnist.mnist --config-file ./test/config-files/ci.toml
+#!/bin/bash
+set -e
 
+export WANDB_MODE=dryrun
+
+# Capture the output of the script in a variable
+output=$(python -m RecurrentFF.benchmarks.mnist.mnist --config-file ./test/config-files/ci.toml)
+
+# Extract the accuracy from the output
+accuracy=$(echo "$output" | grep "test accuracy" | awk -F': ' '{print $NF}')
+accuracy=${accuracy%\%}  # Remove the % sign from the accuracy
+echo "Test Accuracy: $accuracy%"
+
+# Compare the accuracy to the threshold
+threshold=87.7
+if (( $(echo "$accuracy >= $threshold" | bc -l) )); then
+    echo "Test passed. Accuracy ($accuracy%) is above the threshold ($threshold%)."
+else
+    echo "Test failed. Accuracy ($accuracy%) is below the threshold ($threshold%)."
+    exit 1
+fi
--- test/scripts/smoke.sh
+++ test/scripts/smoke.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 # Run benchmarks not needing data files
 export WANDB_MODE=dryrun
 python -m RecurrentFF.benchmarks.mnist.mnist --config-file ./test/config-files/smoke.toml
