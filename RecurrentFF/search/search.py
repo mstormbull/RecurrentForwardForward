86cc0b93ed83b460ce039783d68b0c1815006ce0
Andrew <andrew.smith.recruiting@gmail.com>
2023-08-24 23:10:36 -0700

    fix damping

--- RecurrentFF/search/search.py
+++ RecurrentFF/search/search.py
@@ -81,7 +81,7 @@ if __name__ == "__main__":
 
     train_batch_sizes = [200, 500, 1000, 2000]
     densities = [1]
-    damping_factors = [0.1, 0.2, 0.3, 0.4, 0.5]
+    damping_factors = [0.3, 0.4, 0.5, 0.6, 0.7]
 
     seen = set()
 
