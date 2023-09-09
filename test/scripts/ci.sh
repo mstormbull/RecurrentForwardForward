5748b0ffdeeaa68af784cce22a309f7c0be2a47c
Andrew <andrew.smith.recruiting@gmail.com>
2023-09-09 00:19:01 -0700

    [ci]: broken file

--- test/scripts/ci.sh
+++ test/scripts/ci.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -e
 
+
+# Execute the Python command and capture its exit status
 export WANDB_MODE=dryrun
-python -m RecurrentFF.benchmarks.mnist.mnist --config-file ./test/config-files/ci.toml > log.txt 2>&1
+python -m RecurrentFF.benchmarks.mnist.mnist --config-file ./test/config-files/ci.toml > log.txt 2>&1 || python_exit_code=$?
+
+# If the Python command failed, display the log and exit
+if [ -n "$python_exit_code" ]; then
+    cat log.txt
+    exit $python_exit_code
+fi
+
+cat log.txt
 
 # Extract the accuracy from the log
 accuracy=$(grep "Test accuracy" log.txt | awk -F': ' '{print $NF}')
