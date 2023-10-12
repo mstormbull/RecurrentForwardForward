03368cbd8c12c11a974224b6169ab25684fbe680
Andrew Smith <andsmi@microsoft.com>
2023-10-11 23:55:26 -0700

    [chore]: activation collection pre-accuracy check (#58)
    Perform a precheck before collecting the activations.

--- RecurrentFF/analysis/data_pipeline/collect_activations.py
+++ RecurrentFF/analysis/data_pipeline/collect_activations.py
@@ -10,7 +10,7 @@ from RecurrentFF.settings import Settings, DataConfig
 TEST_BATCH_SIZE = 1
 NUM_BATCHES = 1000
 
-TENSOR_PATH = ""
+WEIGHTS_PATH = ""
 
 if __name__ == "__main__":
     settings = Settings.new()
@@ -42,7 +42,14 @@ if __name__ == "__main__":
     model = RecurrentFFNet(settings).to(settings.device.device)
 
     model.load_state_dict(torch.load(
-        TENSOR_PATH, map_location=settings.device.device))
+        WEIGHTS_PATH, map_location=settings.device.device))
+
+    _train_loader, test_loader_tmp = MNIST_loaders(
+        settings.data_config.train_batch_size, 1000)
+    model.predict(DataScenario.StaticSingleClass,
+                  test_loader_tmp, 1, write_activations=False)
+
+    input("Does the accuracy look good?")
 
     model.predict(DataScenario.StaticSingleClass,
                   test_loader, NUM_BATCHES, write_activations=True)
