39129acd9efb8e5b777ea23235f63b0ede585ff0
Andrew <andrew.smith.recruiting@gmail.com>
2023-11-18 09:58:23 -0800

    confused

--- RecurrentFF/analysis/data_pipeline/collect_activations.py
+++ RecurrentFF/analysis/data_pipeline/collect_activations.py
@@ -10,7 +10,7 @@ from RecurrentFF.settings import Settings, DataConfig
 TEST_BATCH_SIZE = 1
 NUM_BATCHES = 1000
 
-WEIGHTS_PATH = "/home/andrew/Documents/projects/RecurrentForwardForward-search/MNIST_2023-11-15_22-08-38_AGUO5M.pth"
+WEIGHTS_PATH = "/home/andrew/Documents/projects/RecurrentForwardForward-search/MNIST_2023-11-17_11-25-05_WWRSTI.pth"
 
 if __name__ == "__main__":
     settings = Settings.new()
--- RecurrentFF/analysis/seaborn/l2_norm_activations_truncated_filtered.py
+++ RecurrentFF/analysis/seaborn/l2_norm_activations_truncated_filtered.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-DATAFRAME_PATH = "./converted_data.parquet"
+DATAFRAME_PATH = "./converted_data_spike_elim_rescaled_2_250.parquet"
 FILTER_CLASS = 3
 
 # Load the parquet file
@@ -51,7 +51,7 @@ def plot_activations_l2_over_time_from_df(df):
         )
         g._legend.set_bbox_to_anchor((1.05, 0.5))
 
-        g.set(ylim=(0, 250))
+        g.set(ylim=(0, 80))
         g.set(xlim=(0, 25))
 
         # Enhancements
